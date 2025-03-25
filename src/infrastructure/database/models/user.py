af743d24e1cf8f5bb28051ae46fcc154ffc656e0
tighwm <partnyor117@gmail.com>
2025-03-25 17:52:47 +0200

    first models to database

---
+++ src/infrastructure/database/models/user.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import func, DateTime, BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.infrastructure.database import Base
+
+
+class User(Base):
+
+    @staticmethod
+    def get_utc_now():
+        return datetime.now(tz=timezone.utc)
+
+    username: Mapped[str | None] = mapped_column(nullable=True)
+    tg_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
+    registered_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        default=get_utc_now,
+    )
+    balance: Mapped[int] = mapped_column(
+        nullable=False,
+        default=100,
+        server_default="100",
+    )
