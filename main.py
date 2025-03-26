bd5b6624629640ad844a243881c85661c6b23de9
tighwm <partnyor117@gmail.com>
2025-03-26 15:53:37 +0200

    improvements for cleaner

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import asyncio
 
-from src.infrastructure.telegram.bot import TelegramBot
+from aiogram import Bot
+from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.client.default import DefaultBotProperties
 
-tgbot = TelegramBot()
+from src.infrastructure.config import settings
+from src.infrastructure.telegram.bot import AiogramBot
+
+bot = AiogramBot(
+    bot=Bot(
+        token=settings.bot.token,
+        default=DefaultBotProperties(),
+    ),
+    storage=RedisStorage.from_url(str(settings.redis.url)),
+)
 
 
 async def main():
-    await tgbot.start_polling()
+    await bot.start_polling()
 
 
 if __name__ == "__main__":
--- src/infrastructure/telegram/bot.py
+++ src/infrastructure/telegram/bot.py
@@ -1,19 +1,22 @@
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.fsm.strategy import FSMStrategy
 from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.fsm.storage.mongo import MongoStorage
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.base import BaseStorage
+from aiogram.fsm.strategy import FSMStrategy
 
-from src.infrastructure.config import settings
 
-
-class TelegramBot:
-    def __init__(self):
-        self.bot = Bot(
-            token=settings.bot.token,
-            default=DefaultBotProperties(),
-        )
-        self.storage = RedisStorage.from_url(str(settings.redis.url))
-        self.dp = Dispatcher(storage=self.storage, fsm_strategy=FSMStrategy.CHAT)
+class AiogramBot:
+    def __init__(
+        self,
+        bot: Bot,
+        storage: (
+            BaseStorage | MemoryStorage | RedisStorage | MongoStorage | None
+        ) = None,
+    ):
+        self.bot = bot
+        self.storage = storage
+        self.dp = Dispatcher(storage=storage, fsm_strategy=FSMStrategy.CHAT)
 
     async def start_polling(self):
         await self.dp.start_polling(self.bot)
