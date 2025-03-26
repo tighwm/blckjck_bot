f1ec5fb529548068205b711896fe0c9b631ca755
tighwm <partnyor117@gmail.com>
2025-03-26 13:29:35 +0200

    create player entities

---
+++ src/domain/entities/player.py
@@ -0,0 +1,41 @@
+from enum import Enum
+from dataclasses import dataclass, field
+
+from src.domain.entities.card import Card, Rank
+
+
+class PlayerResult(Enum):
+    WIN = "win"
+    LOSE = "lose"
+    OUT = "out"
+
+
+@dataclass
+class Player:
+    username: str | None
+    user_tg_id: int
+    bid: int = 0
+    cards: list[Card] = field(default_factory=list)
+    score: int = 0
+    result: PlayerResult | None = None
+
+    def calculate_score(self) -> int:
+        score = sum(card.get_value() for card in self.cards)
+
+        # Обработка тузов
+        aces_count = sum(1 for card in self.cards if card.rank == Rank.ACE)
+        while score > 21 and aces_count > 0:
+            score -= 10
+            aces_count -= 1
+
+        return score
+
+    @property
+    def score(self):
+        return self.calculate_score()
+
+    def has_blackjack(self) -> bool:
+        return len(self.cards) == 2 and self.calculate_score() == 21
+
+    def is_busted(self) -> bool:
+        return self.calculate_score() > 21
