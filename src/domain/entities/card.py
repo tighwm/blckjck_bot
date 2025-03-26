eb1070728e5c963aa57d53452a817718bb1644de
tighwm <partnyor117@gmail.com>
2025-03-26 13:29:25 +0200

    create card entities

---
+++ src/domain/entities/card.py
@@ -0,0 +1,42 @@
+from enum import Enum
+from dataclasses import dataclass
+
+
+class Rank(str, Enum):
+    TWO = "2"
+    THREE = "3"
+    FOUR = "4"
+    FIVE = "5"
+    SIX = "6"
+    SEVEN = "7"
+    EIGHT = "8"
+    NINE = "9"
+    TEN = "10"
+    JACK = "J"
+    QUEEN = "Q"
+    KING = "K"
+    ACE = "A"
+
+
+class Suit(str, Enum):
+    HEARTS = "hearts"
+    DIAMONDS = "diamonds"
+    CLUBS = "clubs"
+    SPADES = "spades"
+
+
+@dataclass(frozen=True)
+class Card:
+    rank: Rank
+    suit: Suit
+
+    def get_value(self) -> int:
+        if self.rank in {Rank.JACK, Rank.QUEEN, Rank.KING}:
+            return 10
+        elif self.rank == Rank.ACE:
+            return 11
+        else:
+            return int(self.rank)
+
+    def __str__(self) -> str:
+        return f"{self.rank.value}{self.suit.value[0]}"
