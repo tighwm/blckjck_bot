44b3d232e8bc52b36608db9223b0bf684753e1a1
tighwm <partnyor117@gmail.com>
2025-03-28 13:59:54 +0200

    redis cache repo (lobby and game)

---
+++ src/infrastructure/repositories/redis_game_cache_repo.py
@@ -0,0 +1,44 @@
+from redis.asyncio import Redis
+
+from src.application.interfaces.cache_game_repo_interface import CacheGameRepoInterface
+from src.domain.entities.game import Game
+from src.application.schemas.game import GameSchema
+
+
+class RedisGameCacheRepo(CacheGameRepoInterface):
+    def __init__(
+        self,
+        redis: Redis,
+        key_prefix: str = "Game",
+    ):
+        self.redis = redis
+        self.key_prefix = key_prefix
+
+    def _get_key(self, chat_id: int) -> str:
+        return f"{self.key_prefix}:{chat_id}"
+
+    async def cache_game(
+        self,
+        game: Game,
+        exp: int | None = None,
+    ) -> GameSchema:
+        game_schema = GameSchema.model_validate(game, from_attributes=True)
+        key = self._get_key(game_schema.chat_id)
+
+        await self.redis.set(
+            name=key,
+            value=game_schema.model_dump_json(),
+            ex=exp,
+        )
+
+        return game_schema
+
+    async def get_cache_game(self, chat_id: int) -> GameSchema:
+        key = self._get_key(chat_id)
+        data = await self.redis.get(key)
+        game_schema = GameSchema.model_validate_json(data)
+        return game_schema
+
+    async def delete_cache_game(self, chat_id: int) -> None:
+        key = self._get_key(chat_id)
+        await self.redis.delete(key)
---
+++ src/infrastructure/repositories/redis_lobby_cache_repo.py
@@ -0,0 +1,55 @@
+from redis.asyncio import Redis
+
+from src.application.interfaces.cache_lobby_repo_interface import (
+    CacheLobbyRepoInterface,
+)
+from src.domain.entities.lobby import Lobby
+from src.application.schemas.lobby import LobbySchema
+
+
+class RedisLobbyCacheRepo(CacheLobbyRepoInterface):
+    def __init__(
+        self,
+        redis: Redis,
+        key_prefix: str = "Lobby",
+    ):
+        self.redis = redis
+        self.key_prefix = key_prefix
+
+    def _get_key(
+        self,
+        chat_id: int,
+    ):
+        return f"{self.key_prefix}:{chat_id}"
+
+    async def create_lobby(
+        self,
+        lobby: Lobby,
+        exp: int = 180,
+    ) -> LobbySchema:
+        key = self._get_key(lobby.chat_id)
+        lobby_schema = LobbySchema.model_validate(lobby, from_attributes=True)
+
+        await self.redis.set(
+            name=key,
+            value=lobby_schema.model_dump_json(),
+            ex=exp,
+        )
+
+        return lobby_schema
+
+    async def get_lobby(
+        self,
+        chat_id: int,
+    ) -> LobbySchema:
+        key = self._get_key(chat_id)
+        data = self.redis.get(key)
+        lobby_schema = LobbySchema.model_validate_json(data)
+        return lobby_schema
+
+    async def delete_lobby(
+        self,
+        chat_id: int,
+    ):
+        key = self._get_key(chat_id)
+        await self.redis.delete(key)
