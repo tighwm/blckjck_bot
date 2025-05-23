b75c2676aea4447df4c824917c8e43482b2c50b7
tighwm <partnyor117@gmail.com>
2025-05-23 23:46:53 +0300

    added date_bonus and get_bonus method

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -1,9 +1,17 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from typing import TYPE_CHECKING
+
+from domain.types.user.exceptions import (
+    BonusCooldownNotExpired,
+    BonusOnlyBellowFiveBalance,
+)
 
 if TYPE_CHECKING:
     from src.application.schemas import UserSchema
+
+
+BONUS_DELTA = timedelta(days=1)
 
 
 @dataclass
@@ -13,6 +21,7 @@ class User:
     username: str | None
     balance: int
     registered_at: datetime
+    date_bonus: datetime | None
 
     @classmethod
     def from_dto(cls, data: "UserSchema") -> "User":
@@ -22,4 +31,20 @@ class User:
             username=data.username,
             balance=data.balance,
             registered_at=data.registered_at,
+            date_bonus=data.date_bonus,
         )
+
+    def get_bonus(self):
+        if self.balance > 5:
+            raise BonusOnlyBellowFiveBalance()
+
+        now = datetime.now(timezone.utc)
+        delta = now - self.date_bonus
+        if delta >= BONUS_DELTA:
+            self.balance += 125
+            self.date_bonus = now
+        else:
+            raise BonusCooldownNotExpired(
+                message="Too early for bonus.",
+                cooldown=BONUS_DELTA - delta,
+            )
